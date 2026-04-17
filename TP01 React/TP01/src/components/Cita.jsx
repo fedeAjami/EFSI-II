67de3750dfe8564170a26f0889a71649b6ef3ad1
TOBÍAS ILÁN NUL <49194485@ort>
2026-04-17 08:28:49 -0300

    gestor de citas parte 2, cambiado y terminado al tp inicial

--- TP01 React/TP01/src/components/Cita.jsx
+++ TP01 React/TP01/src/components/Cita.jsx
@@ -1,6 +1,13 @@
 import "./Cita.css";
 
 function Cita({ cita, index, eliminarCita }) {
+  const handleEliminar = () => {
+    const confirmar = window.confirm("¿Estás seguro de eliminar esta cita?");
+    if (confirmar) {
+      eliminarCita(index);
+    }
+  };
+
   return (
     <div className="cita">
       <p><strong>Mascota:</strong> {cita.mascota}</p>
@@ -9,7 +16,7 @@ function Cita({ cita, index, eliminarCita }) {
       <p><strong>Hora:</strong> {cita.hora}</p>
       <p><strong>Síntomas:</strong> {cita.sintomas}</p>
 
-      <button onClick={() => eliminarCita(index)}>
+      <button onClick={handleEliminar}>
         ELIMINAR ×
       </button>
     </div>
